7969ec256422b6fd8fdd678bb857c2423247d38b
Pirajeen Kandasamy <pkandasamy7@myseneca.ca>
2025-10-31 11:48:56 -0400

    completed lab5a.py reading data from files

---
+++ lab5a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Author ID: pirajeen Kandasamy
+
+def read_file_string(file_name):
+#Takes file_name (str) and returns the entire file contents as a string.
+    with open(file_name, 'r') as f:
+        return f.read()
+
+def read_file_list(file_name):
+#Takes file_name (str) and returns the file contents as a list of lines with newline characters removed.
+    
+    with open(file_name, 'r') as f:
+        return [line.rstrip('\n') for line in f.readlines()]
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
